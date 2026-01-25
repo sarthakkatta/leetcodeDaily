d81cdec2472d47c185cf0bc3a1f008d3e3c32efe
Sarthak Katta <sarthakkatta7@gmail.com>
2026-01-25 15:50:23 +0530

    Minimize Maximum Pair Sum in Array

--- leetcode1877.cpp
+++ leetcode1877.cpp
@@ -16,7 +16,7 @@ public:
         sort(nums.begin(), nums.end());
         int res = 0;
         int n = nums.size();
-        for (int i = 0; i < n >> 1; i++) {
+        for (int i = 0; i < n >> 1; i++) { 
             res = max(res, nums[n - 1 - i] + nums[i]);
         }
         return res;
